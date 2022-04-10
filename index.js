4f8e240045fd8404f62a84d2c6ad9f7fd81bdfa6
ReitoSerizawa <s.reitiger@gmail.com>
2022-04-10 16:18:25 -0400

    commit tree 3586a3791b30aa9c270187ff25bd929fea500fe2

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+
+const targetedParagraph = document.querySelector('#text')
+
+document.addEventListener('DOMContentLoaded', function () {
+    targetedParagraph.textContent = 'This is really cool!'
+})
+
